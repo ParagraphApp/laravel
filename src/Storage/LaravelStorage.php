23a2537ba781ffcec9cec3783baf282a117eb3f8
Denis Mysenko <denis@mysenko.com>
2021-07-23 15:25:55 +1000

    r fallback

--- src/Storage/LaravelStorage.php
+++ src/Storage/LaravelStorage.php
@@ -9,9 +9,28 @@ class LaravelStorage implements StorageContract {
      */
     public static function loadTranslations($locale = 'default')
     {
-        if (! file_exists(storage_path("pushkin_{$locale}.json"))) return [];
+        $path = storage_path("pushkin_{$locale}.json");
 
-        return json_decode(file_get_contents(storage_path("pushkin_{$locale}.json")), true);
+        if (! file_exists($path) && ! $path = static::fallback($locale)) return [];
+
+        return json_decode(file_get_contents($path), true);
+    }
+
+    /**
+     * @param $locale
+     * @return string
+     */
+    protected static function fallback($locale)
+    {
+        $files = scandir(dirname(storage_path("pushkin.json")));
+
+        $matches = array_filter($files, function($path) use ($locale) {
+            return preg_match('/^pushkin_'.$locale.'_[A-Z]{2}\.json$/', basename($path));
+        });
+
+        if (count($matches)) {
+            return storage_path(array_values($matches)[0]);
+        }
     }
 
     public static function saveTranslations($locale = 'default', $translations)
