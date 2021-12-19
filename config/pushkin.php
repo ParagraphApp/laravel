e648ba3a16a45118a3abb07b9c0742d74e478bff
Denis Mysenko <denis@mysenko.com>
2021-12-19 14:23:40 +1100

    + confnig

---
+++ config/pushkin.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'api_url' => env('PUSHKIN_API_URL', 'https://api.pushkin.com.au'),
+    'api_key' => env('PUSHKIN_API_KEY'),
+    'project_id' => env('PUSHKIN_PROJECT_ID'),
+];
