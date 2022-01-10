300b08332895508f2c1b7462d3e5a3e0d9c4df57
Denis Mysenko <denis@mysenko.com>
2022-01-10 15:51:11 +1100

    r domain

--- config/pushkin.php
+++ config/pushkin.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'api_url' => env('PUSHKIN_API_URL', 'https://api.pushkin.com.au'),
+    'api_url' => env('PUSHKIN_API_URL', 'https://api.pushkin.sh'),
     'api_key' => env('PUSHKIN_API_KEY'),
     'project_id' => env('PUSHKIN_PROJECT_ID'),
 ];
