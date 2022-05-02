c80e77445d1feb3d3eaaf28c6fdd9cb81b76d400
Denis Mysenko <denis@mysenko.com>
2022-05-02 22:12:00 +1000

    r laravel

--- src/ProxyTranslator.php
+++ src/ProxyTranslator.php
@@ -2,7 +2,8 @@
 
 namespace Paragraph;
 
-use \Illuminate\Contracts\Translation\Translator;
+use Illuminate\Contracts\Translation\Translator;
+use Paragraph\Storage\LaravelStorage;
 
 class ProxyTranslator implements Translator {
     /**
@@ -10,13 +11,46 @@ class ProxyTranslator implements Translator {
      */
     protected $laravelTranslator;
 
+    protected $loaded = [];
+
     public function __construct($laravelTranslator)
     {
         $this->laravelTranslator = $laravelTranslator;
     }
 
+    /**
+     * @param $translations
+     * @param $locale
+     */
+    protected function loadTranslations($translations, $locale)
+    {
+        $normalised = [];
+
+        foreach ($translations as $translation) {
+            $key = preg_match('/\./', $translation['placeholder']) ? $translation['placeholder'] : '*.' . $translation['placeholder']; 
+            $normalised[$key] = $translation['text'];
+        }
+
+        $this->laravelTranslator->addLines($normalised, $locale);
+
+        $this->loaded[] = $locale;
+    }
+
+    /**
+     * @param string $key
+     * @param array $replace
+     * @param null $locale
+     * @return mixed|string
+     */
     public function get($key, array $replace = [], $locale = null)
     {
+        $locale = $locale ?: $this->laravelTranslator->getLocale();
+
+        if (! in_array($locale, $this->loaded)) {
+            $translations = LaravelStorage::loadTranslations($locale);
+            $this->loadTranslations($translations, $locale);
+        }
+
         $translation = $this->laravelTranslator->get($key, $replace, $locale);
 
         return is_string($translation) ? p($translation) : $translation;
